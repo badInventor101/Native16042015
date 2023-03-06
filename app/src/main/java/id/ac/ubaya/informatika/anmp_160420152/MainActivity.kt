cd7d603c73eaebf474a2ed2376f720aad0f95cc5
adhimm64 <adhimramadhan64@gmail.com>
2023-03-06 23:32:40 +0700

    update feature from adhimm64

--- app/src/main/java/id/ac/ubaya/informatika/anmp_160420152/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_160420152/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // login feature di sini (2) dan tambahan
+        // update feature from adhimm64
     }
 }
