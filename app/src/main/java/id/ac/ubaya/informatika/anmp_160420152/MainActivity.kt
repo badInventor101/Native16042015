fd95350abd641e9c1903b3198a00fadbe7348ef0
badInventor101 <meteorboy3@gmail.com>
2023-03-06 11:28:13 +0700

    ayo kerja 2024

---
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_160420152/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.informatika.anmp_160420152
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
